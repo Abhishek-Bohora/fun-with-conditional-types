1ad241f65e62a40a14b64973ac92334d0ca0f2ce
Abhishek Bohora <abhishek.bohora16@gmail.com>
2023-03-31 21:53:30 +0545

    intersection-union-reduction-1

---
+++ intersection-union-reduction-01.ts
@@ -0,0 +1,20 @@
+type LooseBoolean = "yes" | "no" | 0 | 1;
+
+type StringValues = LooseBoolean & string;
+type NumberValues = LooseBoolean & number;
+
+//filtering out objects from discriminated union
+type Person =
+  | {
+      type: "d";
+      name: "";
+      age: "";
+      speciality: "";
+    }
+  | { type: "d"; name: ""; age: ""; field: "" };
+
+type Doctor = Person & { type: "d" };
+
+//we want to take any hex value as string input and autocomplete on red and blue
+function takeColor(s: (string & {}) | "red" | "blue") {}
+takeColor("");
