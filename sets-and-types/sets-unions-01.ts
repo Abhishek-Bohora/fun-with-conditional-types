b80ec57f8aed0464ada3a775f1b1cb95fc7addb8
Abhishek Bohora <abhishek.bohora16@gmail.com>
2023-03-31 10:26:16 +0545

    sets-unions-01

---
+++ sets-and-types/sets-unions-01.ts
@@ -0,0 +1,42 @@
+//How can i represent an object that is either a Engineer or a Doctor ?
+//      => Use a Union
+
+interface Engineer {
+  name: string;
+  age: string;
+  field: string;
+}
+
+interface Doctor {
+  name: string;
+  age: string;
+  speciality: string;
+}
+
+function printPerson(p: Engineer | Doctor) {
+  /*
+    console.log(p.speciality);
+    we cannot access the property that are not common between these two interfaces beacause p can only be doctor or an engineer
+    this bring us to Narrowing unions
+  */
+  if ("speciality" in p) {
+    //narrowing union
+    //    in typegaurd can only be used with object union type
+    //    for union of primitive type we use typeof operator
+    p.speciality;
+  } else {
+    p.field;
+  }
+}
+
+printPerson({
+  name: "",
+  age: "",
+  field: "",
+});
+
+printPerson({
+  name: "",
+  age: "",
+  speciality: "",
+});
